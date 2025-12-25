9b838763f01fabb763e63664a5e4924f61fc7f2a
Rishikesh Kumar <rishikeshkumar0804@gmail.com>
2025-12-26 00:12:57 +0530

    added TwoSum solution

---
+++ src/dsa/arrays/TwoSum_03.java
@@ -0,0 +1,49 @@
+package dsa.arrays;
+
+/*1. Two Sum
+Easy
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6git diff --cached
+Output: [0,1]*/
+
+import java.util.Arrays;
+
+public class TwoSum_03 {
+    public static void main(String[] args) {
+        int[] arr = {2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(arr, 9)));
+    }
+
+    public static int[] twoSum(int[] arr, int target){
+        for (int i=0; i<arr.length; i++){
+            for (int j=i+1; j<arr.length-1; j++){
+                if (arr[i]+arr[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{0, 0};
+    }
+    public static int[] twoSum2(int[] nums, int target){
+        int n=nums.length;
+        int left=0, right=n-1;
+
+        while(left > right){
+
+        }
+        return new int[]{0, 0};
+    }
+}
